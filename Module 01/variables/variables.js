86a90805098184fa39e7fd30689f7c8b02e5d926
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2024-10-28 14:46:08 -0300

    variables

---
+++ Module 01/variables/variables.js
@@ -0,0 +1,18 @@
+'use strict'; // it helps to write cleaner code, it helps to avoid errors
+// variables
+
+var first = 'Gianni';
+let age = 44;
+const cool = true;
+// different types of variables
+// statement, is a line of code that performs an action, ends with a semicolon
+
+// scoping: where variables are available for use
+// var variables are function scoped, they are available anywhere in the function
+// let and const are block scoped, they are available only in the block where they are defined
+
+//  const como default, let si necesitas reasignar el valor de la variable
+
+// naming conventions for variables: camelCase, don't start with a number, don't use special characters except $ and _, don't use reserved words, don't start with an uppercase letter
+
+
