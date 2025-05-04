ffddab08c51ac363aa6a3b618e4b5d3824c461e1
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-05-04 19:14:32 -0300

    tabs finished

--- Module 06/tabs.js
+++ Module 06/tabs.js
@@ -1,1 +1,37 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelectorAll('.tabs');
+const tabButtons = document.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+const tabPanelsArray = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  // hide others tabs
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+
+  // mark all tab unselected
+  tabButtons.forEach((tab) => {
+    // tab.ariaSelected = false;
+    tab.setAttribute('aria-selected', false);
+  });
+  // mark the clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+
+  // find the tab panel that matches the clicked tab
+  const { id } = event.currentTarget;
+
+  // method 1
+  // const tabPanel = tabs.querySelector(`[aria-labelledby ="${id}"]`);
+  // tabPanel.hidden = false;
+
+  // method 2 - find in the array of tab panels
+  // tab panels es un NodelList, hay que convertirlo a un array
+  const tabPanel = tabPanelsArray.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id,
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick),
+);
