db1f10f3e12d55f41f1544c419d617a15ea5949a
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-08-19 13:31:07 -0300

    getting the video on the page

--- Module 10/Face Detection/pixelated-face.js
+++ Module 10/Face Detection/pixelated-face.js
@@ -1,4 +1,25 @@
-// The face detection does not work on all browsers and operating systems.
-// If you are getting a `Face detection service unavailable` error or similar,
-// it's possible that it won't work for you at the moment.
-console.log('funciona!!!');
+const video = document.querySelector('.webcam');
+const canvas = document.querySelector('.video');
+const ctx = canvas.getContext('2d');
+const faceCanvas = document.querySelector('.face');
+const faceCtx = faceCanvas.getContext('2d');
+const faceDetector = new window.FaceDetector();
+
+console.log(video, canvas, faceCanvas, faceDetector);
+
+// funcion que popula el video del usuario
+async function populateVideo() {
+  const stream = await navigator.mediaDevices.getUserMedia({video: {width: 1280, height: 720}});
+  video.srcObject = stream;
+  await video.play();
+
+  //ajustar el tamaño del video al canvas
+  console.log(video.videoWidth, video.videoHeight);
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
+}
+
+populateVideo();
